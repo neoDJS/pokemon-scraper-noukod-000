7b0492b690a169f53461585c762d0dc2e8ce417f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-12-12 04:51:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  @@all = []
+  attr_accessor :id, :name, :type, :db, :hp
+  def initialize(id:, name:, type:, db: SQLite3::Database.new('db/pokemon.db'), hp: "")
+    self.id = id
+    self.name = name
+    self.type = type
+    self.db = db
+    @@all << self
+  end
+
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon set hp = ? WHERE id = ?", hp, self.id)
+  end
+
+  def self.save(name, type, db)
+    db.execute("Insert into pokemon(name, type) values (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    row = db.execute("SELECT * from pokemon where id = ?", id)[0]
+    self.new(id: row[0], name: row[1], type: row[2])
+  end
+
 end
